e5b64a33afca29288f41fec76bb6ab696f5b32b0
Øyvind Skaar <oyvind.skaar@bekk.no>
2025-02-18 21:59:38 +0100

    Invalid targets

---
+++ tests/Skaar.MockDependencyInjection.Moq.Tests/Fixture/InvalidTargetTests.cs
@@ -0,0 +1,26 @@
+using Shouldly;
+using Skaar.MockDependencyInjection.Exceptions;
+
+namespace Skaar.MockDependencyInjection.Moq.Tests.Fixture
+{
+    public class InvalidTargetTests
+    {
+        [Fact]
+        public void Resolve_FromInterface_Throws()
+        {
+            var fixture = IoC.CreateFixture<ITestTarget>();
+            
+            Should.Throw<TypeCannotBeCreatedException>(() => fixture.Resolve());
+        }    
+        
+        [Fact]
+        public void Resolve_FromAbstractClass_Throws()
+        {
+            var fixture = IoC.CreateFixture<TestTarget>();
+            
+            Should.Throw<TypeCannotBeCreatedException>(() => fixture.Resolve());
+        }
+        public interface ITestTarget;
+        public abstract class TestTarget : ITestTarget;
+    }
+}
