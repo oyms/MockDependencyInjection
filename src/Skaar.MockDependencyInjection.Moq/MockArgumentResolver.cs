e3a534d6465e8c6e4e4304b6c672a7400131eb54
Øyvind Skaar <oyvind.skaar@bekk.no>
2025-08-19 14:47:08 +0200

    1.0.5

--- src/Skaar.MockDependencyInjection.Moq/MockArgumentResolver.cs
+++ src/Skaar.MockDependencyInjection.Moq/MockArgumentResolver.cs
@@ -5,18 +5,19 @@ using System.Diagnostics;
 namespace Skaar.MockDependencyInjection.Moq;
 
 [DebuggerDisplay("{Key}")]
-class MockArgumentResolver(ResolverSpecification key, MoqConfig config = default) : IArgumentResolver
+class MockArgumentResolver(ResolverSpecification key, MoqConfig? config = null) : IArgumentResolver
 {
     private Mock?  _mock;
     public Mock Mock => _mock ??= CreateMock(Key.ArgumentType);
     public object Resolve() => Mock.Object;
+    private MoqConfig Config => config.GetValueOrDefault(new MoqConfig(behavior: MockBehavior.Loose, defaultValueProvider: DefaultValueProvider.Mock));
     public ResolverSpecification Key { get; } = key;
     private Mock CreateMock(Type t)
     {
         var genericType = typeof(Mock<>).MakeGenericType(t);
-        var mock = (Mock)Activator.CreateInstance(genericType, [config.Behavior])!;
-        mock.CallBase = config.CallBase;
-        mock.DefaultValueProvider = config.DefaultValueProvider ?? DefaultValueProvider.Mock;
+        var mock = (Mock)Activator.CreateInstance(genericType, [Config.Behavior])!;
+        mock.CallBase = Config.CallBase;
+        mock.DefaultValueProvider = Config.DefaultValueProvider ?? DefaultValueProvider.Mock;
         return mock;
     }
 }
